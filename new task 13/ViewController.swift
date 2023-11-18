be4ee4cbda38e38b8fd78e68041e9f3ef32d0e5d
jajanidzenini <121045485+jajanidzenini@users.noreply.github.com>
2023-11-18 10:26:44 +0400

    Initial Commit

---
+++ new task 13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  new task 13
+//
+//  Created by niniku on 18.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
